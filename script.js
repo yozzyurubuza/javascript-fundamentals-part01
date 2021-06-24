f35fea772525b8e074fe679205b4b40063b9ba89
yozzyurubuza <matt_ebarle15@yahoo.com>
2021-06-24 20:40:25 +0800

    Added new notes from learnings

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 let js = 'amazing';
-if (js === 'amazing') alert('Javascript is FUN!');
-
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+let firstNamePerson //Camel Case - Javascript naming standard
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+//let jonas&matilda = 'JM'; // Some special characters cannot be used.
+//let new = 27; //Reserved javascript keyword cannot be used
+let $function = 27;
+
+let Person = 'jonas' //Avoid using uppercase in the start of variable names
+
+//write variable names descriptively
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+console.log(myFirstJob);
