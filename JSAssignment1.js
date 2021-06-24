8c74830d7874d87dd295bf2059d65eab5c44ad50
yozzyurubuza <matt_ebarle15@yahoo.com>
2021-06-24 20:53:37 +0800

    Added Assignments for Course Practice

---
+++ JSAssignment1.js
@@ -0,0 +1,15 @@
+//Assignment for Javascript Fundamentals Part - 1
+//Lecture: Values and Variables
+
+let country = 'Philippines';
+let continent = 'Asia';
+let population = 1000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+//Lecture: Data Types
+
+
+
