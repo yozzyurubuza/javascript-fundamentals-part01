d47a1b7b3fe0e226aebcef73a91f1f524982054a
yozzyurubuza <matt_ebarle15@yahoo.com>
2021-06-28 21:18:25 +0800

    Added Lecture of Operator Precedence

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ console.log(lastName);
 //Math Operators
 const now = 2037;
 const ageJonas = now - 1991;
-const ageSarah = now - 2020
+const ageSarah = now - 2020;
 console.log(ageJonas, ageSarah);
 
 console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
@@ -103,6 +103,22 @@ const isFullAge = ageSarah >= 18;
 console.log(now - 1991 > now - 2018);
 */
 
+//Operator Precedence
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+//console.log(25 - 10 - 5);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
 
 
 
