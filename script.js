29ef0d97c337631c6f17c11f9386c4367ea62268
yozzyurubuza <matt_ebarle15@yahoo.com>
2021-06-28 21:04:46 +0800

    Added Lecture of Basic Operators

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ let myCurrentJob = 'Teacher';
 let job1 = 'programmer';
 let job2 = 'teacher';
 
-console.log(myFirstJob); 
+console.log(myFirstJob);
 
 
 //Data Types
@@ -51,7 +51,6 @@ year = 1991;
 console.log(typeof year);
 
 console.log(typeof null);
-*/
 
 //let, const, and var use cases
 //let - use to mutate the value for later use
@@ -70,6 +69,52 @@ job = 'teacher';
 
 lastName = 'Schmedtmann';
 console.log(lastName);
+
+
+//Basic Operators
+//Math Operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2020
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+//2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'Jonas';
+const lastName = 'Schmedtmann';
+console.log(firstName + ' ' + lastName);
+
+//Assignment Operators
+let x = 10 + 5;
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--; // x = x -1
+x--;
+console.log(x);
+
+//Comparison Operators
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
