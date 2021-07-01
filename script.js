be3b0c343d544e3a78bf23d8f8ad187bb5908cdf
yozzyurubuza <matt_ebarle15@yahoo.com>
2021-07-01 22:17:56 +0800

    Added lecture of Type Conversion and Coercion

--- script.js
+++ script.js
@@ -146,7 +146,7 @@ lines');
 console.log(`String
 multiple
 lines`);
-*/
+
 
 
 //Taking Decisions: If / Else Statements
@@ -170,6 +170,40 @@ if (birthYear <= 2000) {
     century = 21;
 }
 console.log(century);
+*/
+
+//Type Conversion and Coercion
+
+//Conversion = Manually change the data type
+//Coercion = Javascript Automatically change the data type;
+
+//Type Conversion
+const inputYear = '1991';
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number('Jonas'));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+//Type Coercion
+console.log('I am ' + 23 + ' years old');
+console.log('23' - '10' - 3);
+console.log('23' / '2');
+console.log('23' > '18');
+
+let n = '1' + 1;
+n = n - 1;
+console.log(n);
+
+
+
+
+
+
+
+
 
 
 
