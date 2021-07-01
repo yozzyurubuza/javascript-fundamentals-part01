11d68f2dee61f8423499b56e8a94e165951f010e
yozzyurubuza <matt_ebarle15@yahoo.com>
2021-07-01 22:53:48 +0800

    Added lecture of Equality Operators: == vs ===

--- script.js
+++ script.js
@@ -196,7 +196,7 @@ console.log('23' > '18');
 let n = '1' + 1;
 n = n - 1;
 console.log(n);
-*/
+
 
 //Truthy and Falsy Values
 
@@ -221,11 +221,32 @@ if (height) {
 } else {
     console.log('Height is UNDEFINED');
 }
+*/
 
+//Equality Operators: == vs ===
 
+// == does type coercion while === does not.
 
+const age = '18';
+if (age === 18) console.log('You just became an adult :D (strict)');
 
+if (age == 18) console.log('You just became an adult :D (loose)');
 
+const favourite = Number(prompt("What's your favourite number?"));
+console.log(favourite);
+console.log(typeof favourite);
+
+if (favourite === 23) { // 22 === 23 -> FALSE
+    console.log('Cool! 23 is an amazing number');
+} else if (favourite === 7) {
+    console.log('7 is also a cool number');
+} else if (favourite === 9) {
+    console.log('9 is also a cool number');
+} else {
+    console.log('Number is not 23 or 7 or 9');
+}
+
+if (favourite !== 23) console.log('Why not 23?')
 
 
 
