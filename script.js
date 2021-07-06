71eae604635a89286ebff51820603f1cba637a58
yozzyurubuza <matt_ebarle15@yahoo.com>
2021-07-06 09:41:11 +0800

    Added Lecture of The Switch Statement

--- script.js
+++ script.js
@@ -247,7 +247,7 @@ if (favourite === 23) { // 22 === 23 -> FALSE
 }
 
 if (favourite !== 23) console.log('Why not 23?')
-*/
+
 
 //Logical Operators
 
@@ -275,6 +275,73 @@ if (hasDriversLicense && hasGoodVision && !isTired) {
 } else {
     console.log('Someone else should drive...');
 }
+*/
+
+//The Switch Statement
+
+const day = 'friday';
+
+switch (day) {
+    case 'monday': // day === 'monday'
+        console.log('Plan course structure');
+        console.log('Go to coding meetup');
+        break;
+    case 'tuesday':
+        console.log('Prepare theory videos');
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log('Write code examples');
+        break;
+    case 'friday':
+        console.log('Record videos');
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log('Enjoy the weekend :D');
+        break;
+    default:
+        console.log('Not a valid day!');
+}
+
+//Implementation of the example above in if-else
+if (day === "monday") {
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+} else if (day === "tuesday") {
+    console.log('Prepare theory videos');
+} else if (day === "wednesday" || day === "thursday") {
+    console.log('Write code examples');
+} else if (day === "friday") {
+    console.log('Record videos');
+} else if (day === "saturday" || day === "sunday") {
+    console.log('Enjoy the weekend :D');
+} else {
+    console.log('Not a valid day!');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
