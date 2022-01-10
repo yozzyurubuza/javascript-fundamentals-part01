ab654ec3e1f4916ff393037f288619ba3ecf96dc
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-01-11 00:06:47 +0800

    Added Assignment for Javascript Training

--- JSAssignment1.js
+++ JSAssignment1.js
@@ -1,15 +1,74 @@
 //Assignment for Javascript Fundamentals Part - 1
 //Lecture: Values and Variables
 
-let country = 'Philippines';
-let continent = 'Asia';
-let population = 1000000;
+const country = "Philippines";
+const continent = "Asia";
+let population = 1;
 
 console.log(country);
 console.log(continent);
 console.log(population);
 
+console.log("////////////////////");
 //Lecture: Data Types
+const isIsland = false;
+let language;
 
+console.log(
+  typeof isIsland,
+  typeof population,
+  typeof country,
+  typeof language
+);
+console.log("////////////////////");
+//Lecture: let, const, and var
+language = "Tagalog";
+//isIsland = true; - error
 
+console.log("////////////////////");
+//Lecture: Basic Operators
+const splitCountry = population / 2;
+console.log(population + 1);
+const finlandPopulation = 6;
+const isFinlandPopulationHigher = finlandPopulation > population;
+const averagePopulation = 33;
+const isCountryPopulationLessAverage = population < 33;
+const description =
+  country +
+  " is in " +
+  continent +
+  ", and its " +
+  population +
+  " million people speak " +
+  language;
+console.log(description);
 
+console.log("////////////////////");
+//Strings and Template Literals
+const description2 = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
+console.log(description2);
+
+//Taking Decisions: if / else statements
+
+// const countryPopulation =
+//   population > averagePopulation
+//     ? `${country}'s population is above average`
+//     : `${country}'s population is ${
+//         averagePopulation - population
+//       } million below average`;
+
+// console.log(countryPopulation);
+
+if (population > averagePopulation) {
+  console.log(`${country}'s population is above average`);
+} else {
+  console.log(
+    `${country}'s population is ${
+      averagePopulation - population
+    } million below average`
+  );
+}
+
+// population = 13
+// population = 130
+console.log("////////////////////");
