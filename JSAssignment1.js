19ce93c3db124f719c10d4eb8fa00306aa9de7be
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-01-11 16:56:14 +0800

    Completed Assignment for Part 1

--- JSAssignment1.js
+++ JSAssignment1.js
@@ -72,3 +72,88 @@ if (population > averagePopulation) {
 // population = 13
 // population = 130
 console.log("////////////////////");
+//Type Conversion and Coercion
+
+/*
+1. Predict the result of these 5 operations without executing them:
+'9' - '5'; = 4
+'19' - '13' + '17'; = 617
+'19' - '13' + 17; = 23
+'123' < 57; = False
+5 + 6 + '4' + 9 - 4 - 2; = 1143
+*/
+//Note: Last Coerced data type will be the current
+
+console.log(5 + 6 + "4" + 9 - 4 - 2);
+console.log("////////////////////");
+//Equality Operators: == vs ===
+
+// const numNeighbours = prompt(
+//   "How many neighbour countries does your country have?"
+// );
+
+// const numNeighbours = Number(prompt(
+//     "How many neighbour countries does your country have?"
+//   ));
+
+// if (numNeighbours === 1) {
+//   console.log("Only 1 border");
+// } else if (numNeighbours > 1) {
+//   console.log("More than 1 border");
+// } else {
+//   console.log("No borders!");
+// }
+
+//When using prompt, input is a string and == coerced it into a number, while using strict will not coerce the string 1 into number, so it goes to the else block. Placing prompt() inside Number() converts the input into a number
+console.log("////////////////////");
+//Logical Operators
+
+// language = "english";
+
+const newCountry =
+  language == "english" && population < 50
+    ? console.log(`You should live in ${country} :)`)
+    : console.log(`${country} does not meet your criteria :(`);
+
+console.log("////////////////////");
+//The Switch Statement
+
+// language = prompt("What language do you use?");
+
+switch (language) {
+  case "chinese":
+  case "mandarin":
+    console.log("MOST number of native speakers!");
+    break;
+  case "spanish":
+    console.log("2nd place in number of native speakers");
+    break;
+  case "english":
+    console.log("3rd place");
+    break;
+  case "hindi":
+    console.log("Number 4");
+    break;
+  case "arabic":
+    console.log("5th most spoken language");
+    break;
+  default:
+    console.log("Great language too :D");
+    break;
+}
+
+console.log("////////////////////");
+//The Conditional (Ternary) Operator
+
+// population = 130;
+
+population =
+  population > 33
+    ? console.log(`${country}'s population is above average`)
+    : console.log(`${country}'s population is below average`);
+
+// console.log(
+//   `${country}'s population is ${population > 33 ? "above" : "below"} average`
+// );
+
+console.log("////////////////////");
