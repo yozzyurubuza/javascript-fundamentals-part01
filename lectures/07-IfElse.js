51103d501c3b73660ea517195132777193266570
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-04-27 20:44:18 +0800

    Refactor If-Else lecture

---
+++ lectures/07-IfElse.js
@@ -0,0 +1,21 @@
+//Taking Decisions: If / Else Statements
+
+const age = 15;
+
+//Control Structure
+if (age >= 18) {
+  console.log("Sarah can start driving license.");
+} else {
+  const yearsLeft = 18 - age;
+  console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
+}
+
+const birthYear = 2012;
+
+let century;
+if (birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+console.log(century); //21
