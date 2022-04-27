a27d7a90d8a1bf7097f18d7bdc1ce3e8d44a5f5e
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-04-27 21:26:31 +0800

    Refactor Equality Operators lecture

---
+++ lectures/10-EqualityOperators.js
@@ -0,0 +1,25 @@
+//Equality Operators: == vs ===
+
+// == does type coercion while === does not.
+
+const age = "18";
+if (age === 18) console.log("You just became an adult :D (strict)"); //Triggers if age = 18;
+
+if (age == 18) console.log("You just became an adult :D (loose)");
+
+const favourite = Number(prompt("What's your favourite number?"));
+console.log(favourite); //23
+console.log(typeof favourite); //number
+
+if (favourite === 23) {
+  // 22 === 23 -> FALSE
+  console.log("Cool! 23 is an amazing number");
+} else if (favourite === 7) {
+  console.log("7 is also a cool number");
+} else if (favourite === 9) {
+  console.log("9 is also a cool number");
+} else {
+  console.log("Number is not 23 or 7 or 9");
+}
+
+if (favourite !== 23) console.log("Why not 23?");
