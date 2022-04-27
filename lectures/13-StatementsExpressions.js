d2ce61f1f1180a20971a6673e71a98eec61fc14e
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-04-27 21:45:37 +0800

    Refactor Statements and Expressions lecture

---
+++ lectures/13-StatementsExpressions.js
@@ -0,0 +1,17 @@
+//Statements and Expressions
+
+//Expression = produce a value / word
+//Example:
+// 3 + 4
+// 1991
+// true && false && !false
+
+//Statement = A sequence of actions
+//Example:
+
+// if (23 > 10){
+//     const str = '23 is bigger';
+// }
+
+const me = "Jonas"; // Expression
+console.log(`I'm ${2037 - 1991} years old ${me}.`); // Statement - because it is a sequence of actions (console.log -> Template literal).
--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-/*
-
-
-//Statements and Expressions
-
-//Expression = produce a value / word
-//Example:
-// 3 + 4
-// 1991
-// true && false && !false
-
-//Statement = A sequence of actions
-//Example:
-
-// if (23 > 10){
-//     const str = '23 is bigger';
-// }
-
-const me = 'Jonas';
-console.log(`I'm ${2037 - 1991} years old ${me}.`)
-*/
-
 //The Conditional (Ternary Operator)
 
 //Ternary Operator - an expression that can return a value similar to if else statement.
