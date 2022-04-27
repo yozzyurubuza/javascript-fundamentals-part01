96afccffc3b517ad12a708d12cbb1f4123f1901d
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-04-27 21:48:33 +0800

    Add few notes

--- coding-challenges/codingChallenge4.js
+++ coding-challenges/codingChallenge4.js
@@ -1,4 +1,4 @@
-//Coding Challenge #4
+//Coding Challenge #4 - After lectures 12-14
 
 // Steven wants to build a very simple tip calculator for whenever he goes eating in a restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
 
@@ -14,11 +14,12 @@
 // GOOD LUCK 😀
 
 const bill = 430;
-const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 const totalBill = bill + tip;
 
-console.log(`The bill was ${bill}, the tip was ${tip}, and the total value is ${totalBill}`);
-
+console.log(
+  `The bill was ${bill}, the tip was ${tip}, and the total value is ${totalBill}`
+);
 
 //Jonas Schmedtmann Implementation
 
@@ -26,5 +27,3 @@ console.log(`The bill was ${bill}, the tip was ${tip}, and the total value is ${
 // const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
 
 // console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
-
-
